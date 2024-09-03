d23da52292958d2062907e376402e4c2e02a9eff
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-09-03 11:18:07 -0300

    comit Co-authored-by: tomasriuli <tomasriuli@users.noreply.github.com> Co-authored-by: bianca. <wwwbian@users.noreply.github.com> Co-authored-by: Elmangacadiabolico <Elmangacadiabolico@users.noreply.github.com>

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
-import './App.css'
-
+import Conexion from './Conexion'
 function Buscador() {
+  const [nombre, setNombre] = useState('');
+
+  const handleInputChange = (event) => {
+    setNombre(event.target.value);
+    console.log(nombre)
+  };
+
 
   return (
     <>
-     
+      <div>
+      <input type="text" placeholder="Ingresar Ciudad" value={nombre} onChange={handleInputChange} 
+      />
+      <Conexion nombre={nombre} />
+    </div>
     </>
   )
 }
